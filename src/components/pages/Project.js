d898da4115755f13c3d25292d9554361b719635c
Vida Nsiah <vidansiah@Vidas-MacBook-Air.local>
2023-03-06 20:33:16 +0000

    working on project.js

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,11 +1,17 @@
 import React from "react";
+import readMeGenerator from "../../assets/portfolio/ReadmeGenerator.jpg";
+import TeamProfileGenerator from "../../assets/portfolio/TeamProfileGenerator.jpg";
+import WeatherApp from "../../assets/portfolio/WeatherApp.jpg";
+import FoonDer from "../../assets/portfolio/FoonDER.jpg";
+import WebDesign from "../../assets/portfolio/WebDesign.jpg";
+import WorkDay from "../../assets/portfolio/WorkDay.jpg";
 
 const Project = () => {
   const portfolio = [
-   
-         <p></p>
-  ]
-  
-};
 
+    <p></p>
+    );
+  };
+  
+  export default Project;
 export default Project;
